a8430d8487fdf48cac67f627c7559b1560b0788b
ppgamedevs <ppgamedevs@gmail.com>
2025-09-18 02:27:51 +0300

    next15(go/[id]): params as Promise and await usage

--- apps/web/src/app/go/[id]/page.tsx
+++ apps/web/src/app/go/[id]/page.tsx
@@ -1,4 +1,4 @@
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page({ params }: { params: Promise<{ id: string } }) {
   const __p = await params as any;
 
   const { id } = __p;
